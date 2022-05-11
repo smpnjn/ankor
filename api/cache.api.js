2a9153339d496c83baa853b94b5db3a99fbe7ac5
Johnny Simpson <johnnysimpson@Johnnys-MacBook-Pro.local>
2022-05-11 16:53:28 +0100

    Added check on URL

--- api/cache.api.js
+++ api/cache.api.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import { createClient } from 'redis'
+import path from 'path'
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Index.js
+import dotenv from 'dotenv'
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 // Redis Connection
 const client = createClient();
@@ -16,6 +25,9 @@ cacheApi.post('/cache', async (req, res, next) => {
                 let getUrl = req.body.url.split(process.env.rootUrl.slice(0, -1));
                 if(getUrl.length > 1) {
                     let cacheTerm = getUrl[1];
+                    if(cacheTerm.href === process.env.rootUrl) {
+                        console.log('hey ho');
+                    }
                     if(cacheTerm === '/') {
                         req.cacheTerm = 'root'
                     }
@@ -23,16 +35,22 @@ cacheApi.post('/cache', async (req, res, next) => {
                         req.cacheTerm = cacheTerm;
                     }
                     
-                    let getHtml = await fetch(req.body.url, {
-                        method: "GET",
-                        headers: {
-                            "x-forceCache" : true
-                        }
-                    })
-                    let response = await getHtml.text();
-                    // Redis connection
-                    await client.set(`${req.cacheTerm}`, response);
-                    return res.status(200).send({ "message" : "Recached URL", "url" : cacheTerm });
+                    let checkUrl = url.parse(req.body.url);
+                    if(checkUrl.href === process.env.rootUrl) {
+                        let getHtml = await fetch(req.body.url, {
+                            method: "GET",
+                            headers: {
+                                "x-forceCache" : true
+                            }
+                        })
+                        let response = await getHtml.text();
+                        // Redis connection
+                        await client.set(`${req.cacheTerm}`, response);
+                        return res.status(200).send({ "message" : "Recached URL", "url" : cacheTerm });
+                    }
+                    else {
+                        return res.status(400).send({ "error" : "Invalid URL" });
+                    }
                 }
                 else {
                     return res.status(400).send({ "error" : "Invalid cache URL" })
