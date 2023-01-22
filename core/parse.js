e6e6f43363cff3f7648b00298ba3d93f420875d6
Johnny Simpson <johnnysimpson@Johnnys-MacBook-Pro.local>
2023-01-22 20:09:01 +0000

    BE: revert on documentElement on parseDataTags

--- core/parse.js
+++ core/parse.js
@@ -632,7 +632,7 @@ const parseDataTags = async (template, req, post, cache) => {
 
         console.timeEnd(`parse-data-tags-${timerUuid}`);
 
-        resolve(document.documentElement.outerHTML.replaceAll(/<[\s]*data[\s\S]*?>|<[\s]*array[\s\S]*?>|<\/array>|<\/data[\s\S]*?>/g, ""))
+        resolve(document.body.innerHTML.replaceAll(/<[\s]*data[\s\S]*?>|<[\s]*array[\s\S]*?>|<\/array>|<\/data[\s\S]*?>/g, ""))
     })
 }
 
